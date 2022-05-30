b98388c9926cb98b9ec1c3aca85432f292c5c88e
Duskley <matthias.bocquelet@orange.fr>
2022-05-30 02:19:31 +0200

    ajout de la bibliotheque Antlr4 + resolution probleme module + test

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const fs= require("fs");
-const scoring= require("./scoring/scoring"); 
+import fs from 'fs'
+import antlr4 from 'antlr4'
+import grammarPythonLexer from './grammarPythonLexer.js'
+import grammarPythonParser from './grammarPythonParser.js'
 
-let code= fs.readFileSync("test.js", "utf8");
-let score= scoring.from(code);
-console.log("--------", "Result","--------");
-console.log(score);
+const nameFile = process.argv[2]
 
+var input = fs.readFileSync(nameFile, 'UTF-8')
+var chars = new antlr4.InputStream(input)
+var lexer = new grammarPythonLexer(chars)
+var tokens  = new antlr4.CommonTokenStream(lexer)
+var parser = new grammarPythonParser(tokens)
 
+parser.buildParseTrees = true
+var tree = parser.program()
+
+if (tree.parser._syntaxErrors == 0)
+{
+    console.log("aucune erreurs de syntax trouvé ! ")
+}
+else
+{
+    console.log("une ou plusieurs erreur de syntax ont été trouvé ! ")
+}
