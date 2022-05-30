8ab815c59e3c8cd36efd8980ce5e471b6389bf4d
ngizila <nataliagizila@gmail.com>
2022-05-30 15:54:11 +0200

    get correct error line

--- index.js
+++ index.js
@@ -2,54 +2,62 @@ import fs from 'fs'
 import antlr4 from 'antlr4'
 import grammarPythonLexer from './grammarPythonLexer.js'
 import grammarPythonParser from './grammarPythonParser.js'
-import { type } from 'os'
 
 const nameFile = process.argv[2]
 
 var input = fs.readFileSync(nameFile, 'UTF-8')
 var chars = new antlr4.InputStream(input)
 var lexer = new grammarPythonLexer(chars)
-var tokens  = new antlr4.CommonTokenStream(lexer)
+var tokens = new antlr4.CommonTokenStream(lexer)
 var parser = new grammarPythonParser(tokens)
 var listener = new SyntaxError();
 
 var errTokens = [];
 
 parser.removeErrorListeners();
+
+//gestion en fonction du type d'erreur
 parser.addErrorListener({
-syntaxError: (recognizer, offendingSymbol, line, column, msg, err) => {
-    console.error(`offendingSymbol: ${offendingSymbol}, line ${line}, col ${column}: ${msg}, err: ${err}`);
-    errTokens.push(offendingSymbol)
-}
+    syntaxError: (recognizer, offendingSymbol, line, column, msg, err) => {
+        console.error(`offendingSymbol: ${offendingSymbol}, line ${line}, col ${column}: ${msg}, err: ${err}`);
+        errTokens.push(offendingSymbol)
+    }
 });
 
 
 parser.buildParseTrees = true
 var tree = parser.file_input()
 const totalLines = tree.stop.line;
-const tokenIndex = tree.stop.tokenIndex;
+const tokenIndex = tree.stop.tokenIndex-1;
 
 
-if (tree.parser._syntaxErrors == 0)
-{
+if (tree.parser._syntaxErrors == 0) {
     console.log("aucune erreurs de syntax trouvé ! ")
     console.log("total lines: " + totalLines)
 }
-else
-{
-    console.log("Nombre des erreurs: " + tree.parser._syntaxErrors) 
+else {
+    console.log("Nombre des erreurs: " + tree.parser._syntaxErrors)
     //console.log("une ou plusieurs erreur de syntax ont été trouvé ! ")
     console.log("error name : " + listener.name + " line: " + listener.message)
-    
 
-      for(let err in parser._errHandler){
-        console.log("err: " + err + " " +  parser._errHandler[err])
+
+    for (let err in parser._errHandler) {
+        console.log("err: " + err + " " + parser._errHandler[err])
     }
 }
 
 console.log("****************************")
-for(let err in errTokens){
-    console.log("errToken: " + errTokens[err]) //.line)
+
+for (let err in errTokens) {
+    let errTok =  errTokens[err];
+    let errLine = errTok.line;
+    
+    if(errLine < totalLines || errTok.tokenIndex < tokenIndex)
+    {
+        errLine = errTok.line - 1;
+    }
+        
+    console.log("errToken: " + errTok + " " + "line: " + errLine + " || total: " + totalLines + " || lastTokenIndex: " + tokenIndex)
 }
 console.log("****************************")
 
