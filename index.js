bdbf16d7fd9d5a62d33a97c3b1d37f5f551840ed
ngizila <nataliagizila@gmail.com>
2022-05-30 15:22:13 +0200

    get err line

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs from 'fs'
 import antlr4 from 'antlr4'
 import grammarPythonLexer from './grammarPythonLexer.js'
 import grammarPythonParser from './grammarPythonParser.js'
+import { type } from 'os'
 
 const nameFile = process.argv[2]
 
@@ -10,17 +11,49 @@ var chars = new antlr4.InputStream(input)
 var lexer = new grammarPythonLexer(chars)
 var tokens  = new antlr4.CommonTokenStream(lexer)
 var parser = new grammarPythonParser(tokens)
+var listener = new SyntaxError();
+
+var errLine = [];
+
+parser.removeErrorListeners();
+parser.addErrorListener({
+syntaxError: (recognizer, offendingSymbol, line, column, msg, err) => {
+    console.error(`offendingSymbol: ${offendingSymbol}, line ${line}, col ${column}: ${msg}, err: ${err}`);
+    errLine.push(line)
+}
+});
+
 
 parser.buildParseTrees = true
 var tree = parser.file_input()
+const totalLines = tree.stop.line;
+const tokenIndex = tree.stop.tokenIndex;
+
 
 if (tree.parser._syntaxErrors == 0)
 {
     console.log("aucune erreurs de syntax trouvé ! ")
-    let totalLines = tree.stop.line;
     console.log("total lines: " + totalLines)
 }
 else
 {
-    console.log("une ou plusieurs erreur de syntax ont été trouvé ! ")
+    console.log("Nombre des erreurs: " + tree.parser._syntaxErrors) 
+    //console.log("une ou plusieurs erreur de syntax ont été trouvé ! ")
+    console.log("error name : " + listener.name + " line: " + listener.message)
+    
+
+      for(let err in parser._errHandler){
+        console.log("err: " + err + " " +  parser._errHandler[err])
+    }
 }
+
+console.log("****************************")
+console.log("errLine: " + errLine)
+
+// console.log("TOKENS: ")
+// let index = 0;
+// while(index < tokenIndex-1){
+//     console.log(index + " : " + tokens.get(index))
+//     index++;
+// }
+
