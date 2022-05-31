f6e96f0e028b11b9cf386db106899b4ee914caba
ngizila <nataliagizila@gmail.com>
2022-05-31 09:50:30 +0200

    simple deadcode minor code detector

--- index.js
+++ index.js
@@ -10,20 +10,27 @@ var chars = new antlr4.InputStream(input)
 var lexer = new grammarPythonLexer(chars)
 var tokens = new antlr4.CommonTokenStream(lexer)
 var parser = new grammarPythonParser(tokens)
-var listener = new SyntaxError();
 
 var errTokens = [];
 
 parser.removeErrorListeners();
+var minorErr = 0;
+var deadCode = 0;
+var majorErr = 0;
+var errorList = [];
 
 //gestion en fonction du type d'erreur
 parser.addErrorListener({
     syntaxError: (recognizer, offendingSymbol, line, column, msg, err) => {
         console.error(`offendingSymbol: ${offendingSymbol}, line ${line}, col ${column}: ${msg}, err: ${err}`);
         errTokens.push(offendingSymbol)
+
+        // console.log("################################## " + recognizer['_input'])
+        // for(let el in recognizer){
+        //     console.log("!!!!!!EL: " + el)
+        // }
     }
 });
-
 
 parser.buildParseTrees = true
 var tree = parser.file_input()
@@ -37,8 +44,6 @@ if (tree.parser._syntaxErrors == 0) {
 }
 else {
     console.log("Nombre des erreurs: " + tree.parser._syntaxErrors)
-    //console.log("une ou plusieurs erreur de syntax ont été trouvé ! ")
-    console.log("error name : " + listener.name + " line: " + listener.message)
 
 
     for (let err in parser._errHandler) {
@@ -46,26 +51,113 @@ else {
     }
 }
 
-console.log("****************************")
+// console.log("****************************")
 
-for (let err in errTokens) {
-    let errTok =  errTokens[err];
-    let errLine = errTok.line;
+// for (let err in errTokens) {
+//     let errTok =  errTokens[err];
+//     let errLine = errTok.line;
     
-    if(errLine < totalLines || errTok.tokenIndex < tokenIndex)
-    {
-        errLine = errTok.line - 1;
-    }
+//     if(errLine < totalLines || errTok.tokenIndex < tokenIndex)
+//     {
+//         errLine = errTok.line - 1;
+//     }
         
-    console.log("errToken: " + errTok + " " + "line: " + errLine + " || total: " + totalLines + " || lastTokenIndex: " + tokenIndex)
+//     console.log("errToken: " + errTok + " " + "line: " + errLine + " || total: " + totalLines + " || lastTokenIndex: " + tokenIndex)
+// }
+// console.log("****************************")
+
+
+console.log("ERROR DETECTOR: ")
+
+let printErrLine = -1;
+for(let err in errTokens){
+    let errTok =  errTokens[err];
+    
+    let index = 0;
+    let isPrint = false;
+    
+    while(index < tokenIndex-1){
+        let token = tokens.get(index);
+        
+        if(token.line == errTok.line && printErrLine != token.line){
+            
+            if(token.text.includes("print")){
+                isPrint = true;
+                minorErr++;
+            }
+            if(isPrint){
+                printErrLine = token.line;
+            }
+        }
+        index++;
+
+    }
+
 }
-console.log("****************************")
+
+console.log("DEADCODE")
+
+let inDef = false;
+let returnUsed = false;
+let lastDeadCodeLine = -1;
+let returnLine = 0;
+let defCounter = 0;
+let index = 0;
+while(index < tokenIndex-1){
+    let token = tokens.get(index);
+    
+    if(token.text == "def"){
+        inDef = true;
+        defCounter++;
+    }
+
+    if(token.text == "return"){
+        returnUsed = true;
+        returnLine = token.line;
+    }
+
+    if(token.text.includes("\n")){
+        returnUsed = false;
+        inDef = false;
+        defCounter--;
+    }
+
+    if(returnUsed == true && returnLine < token.line  && token.text.trim() != ''){
+        
+        if(lastDeadCodeLine != token.line){
+            lastDeadCodeLine = token.line;
+            deadCode++;
+        }
+        lastDeadCodeLine = token.line;
+        
+    }
+    index++;
+}
+
+console.log("DEADCODE ERR: " + deadCode)  //p.ex un code après return
+console.log("MINOR ERR: " + minorErr) // p.ex un var sans declarator
 
 
-// console.log("TOKENS: ")
-// let index = 0;
 // while(index < tokenIndex-1){
+
+//     let token = tokens.get(index);
+//     if(token.line == line){
+//         if("print" in token.text){
+//             printErr = true;
+//             minorErr++;
+//         }
+
+
+//     }
 //     console.log("Index: " + index + " : " + tokens.get(index))
 //     index++;
 // }
+
+
+console.log("TOKENS: ")
+index = 0;
+while(index < tokenIndex-1){
+    console.log("Index: " + index + " : " + tokens.get(index))
+    index++;
+}
 
--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+def test():
+    x = 10
+    if( 10 == 10):
+        if(1==1):
+            x = 11
+        return 5
+        ggg = 1 
+        kkk = 1
+    return " val "
 
-x := 10 + 5
-
-print x + 8
-
-print return
+print test()
